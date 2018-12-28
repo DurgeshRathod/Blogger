3705cfe0a297c1ef608af2bd80550cdb26a2b1bc
Durgesh Rathod <durgeshrathod.777@gmail.com>
2018-12-28 16:34:44 +0530

    Dockerized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BlogpostRoutingModule } from './blogpost/blogpost-routing.module'
+import { BlogpostModule } from './blogpost/blogpost.module';
+import { CmspageModule } from './cmspage/cmspage.module';
+import { HttpClientModule } from '@angular/common/http'
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    BlogpostModule,
+    CmspageModule,
+    BlogpostRoutingModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [Title],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
